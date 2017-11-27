2d3059675febe4930c72f069d0d7a2cc6a34ea6e
eerovil <eero.vilpponen@gmail.com>
2017-11-27 10:56:34 +0200

    initial test build

---
+++ js/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+// Sample exports from other modules
+__export(require("./lib/formulas"));
+// Sample export of our own
+exports.bas = "123";
---
+++ ts/index.ts
@@ -0,0 +1,5 @@
+// Sample exports from other modules
+export * from "./lib/formulas";
+
+// Sample export of our own
+export var bas = "123";
